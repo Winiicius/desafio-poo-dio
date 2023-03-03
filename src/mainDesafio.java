b419bc0a58295c5fdfcb63ec191a7df9ba7303f9
Winiicius <102719335+Winiicius@users.noreply.github.com>
2023-03-03 15:01:44 -0300

    Add files via upload

--- src/mainDesafio.java
+++ src/mainDesafio.java
@@ -3,12 +3,33 @@ import java.time.LocalDate;
 import br.com.dio.desafio.dominio.*;
 
 public class mainDesafio {
-    public static void main(String[] args){
+    public static void main(String[] args){ 
         
-        Curso curso1 = new Curso("Curso Java", "Aprender Java", 20);
-        System.out.println(curso1);
+        Curso cursoJava = new Curso("Curso Java", "Aprender Java", 20);
+        Curso cursoPython = new Curso("Curso Python", "Aprender Python", 15);
 
         Mentoria mentoria1 = new Mentoria("Mentoria Java", "Mentoria java", LocalDate.now());
-        System.out.println(mentoria1);
+
+        Bootcamp bootcampJava = new Bootcamp("Dio Bootcamp", "Aprendendo Java", 30);
+        Bootcamp bootcampPython = new Bootcamp("Dio Bootcamp", "Aprendendo Python", 25);
+
+        bootcampJava.getConteudos().add(cursoJava);
+        bootcampJava.getConteudos().add(mentoria1);
+        bootcampPython.getConteudos().add(cursoPython);
+
+        Dev dev1 = new Dev("Winicius");
+        Dev dev2 = new Dev("Marcos");
+        
+        dev1.inscreverBootcamp(bootcampPython);
+        dev1.progredir();
+        
+        dev1.inscreverBootcamp(bootcampJava);
+        System.out.println("XP Winicius: " + dev1.calcularTotalXP());
+
+        dev2.inscreverBootcamp(bootcampPython);
+        System.out.println("XP Marcos: " + dev2.calcularTotalXP());
+
+
+
     }
 }
